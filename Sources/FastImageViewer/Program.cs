a19f75282a6a340d17be38449688a928d5f2719b
Ivan Kasyanov <ivan.vl.kasyanov@gmail.com>
2025-10-21 00:19:35 +0200

    Setup.

---
+++ Sources/FastImageViewer/Program.cs
@@ -0,0 +1,6 @@
+// <copyright file="Program.cs" company="Ivan Kasyanov">
+// © 2025 Ivan Kasyanov.
+// This software is licensed under GNU Affero General Public License Version 3. See LICENSE for details.
+// </copyright>
+
+Console.Write("Hello, World!");
